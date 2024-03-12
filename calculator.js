c6620ee5fd6ce4f8ae9e7a34bde95f14ebbff624
Marvolox <dalmaso307@gmail.com>
2024-03-12 05:04:35 +0300

    added javascript and the function

--- calculator.js
+++ calculator.js
@@ -1,1 +1,10 @@
-let length = document
+function calculate() {
+
+let length = document.getElementById("length")
+
+let width = document.getElementById("width")
+
+let area = lenth*width
+
+let perimwter = 2*(length+width)
+}
