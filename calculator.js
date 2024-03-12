b7fae76b75b4b562e2f2d6c644cb9e82f360d978
Marvolox <dalmaso307@gmail.com>
2024-03-12 09:28:05 +0300

    added the if argument

--- calculator.js
+++ calculator.js
@@ -4,7 +4,18 @@ let length = document.getElementById("length")
 
 let width = document.getElementById("width")
 
+if (length === '' || width === '' || length <= 0 || width <= 0) {
+    document.getElementById('result').innerHTML = '<p>Please enter valid values for length and width.</p>';
+    return;
+  }
+
 let area = lenth*width
 
-let perimwter = 2*(length+width)
+let perimeter = 2*(length+width)
+
+document.getElementById('result').innerHTML = `
+<p>Area: ${area}</p>
+<p>Perimeter: ${perimeter}</p>
+`;
+
 }
